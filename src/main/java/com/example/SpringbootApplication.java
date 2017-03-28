3b2b64cf7033bc302c8120ba0eeebc254b2d2dbd
at <at@DESKTOP-ST54INN>
2017-03-28 23:57:44 +0800

    init

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,10 +10,10 @@ public class SpringbootApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.sources(SpringbootApplication.class);
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
